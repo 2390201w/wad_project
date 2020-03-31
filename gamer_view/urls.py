f2c2de80b62b3d47d4d9a42e9c61434c18ded325
Yuhao Wu <2390201w@student.gla.ac.uk>
2020-03-31 18:04:09 +0100

    Register finished, not tested

--- gamer_view/urls.py
+++ gamer_view/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 path('', views.Home, name='Home'),
 path('AboutUs/', views.AboutUs, name='AboutUs'),
 path('Trending/', views.Trending, name='Trending'),
+path('register/',views.register, name='register'),
 ]
 
--- gamer_view/views.py
+++ gamer_view/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from gamer_view.forms import UserForm, UserProfileForm
+from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here
@@ -15,3 +18,57 @@ def AboutUs (request):
 
 def Trending(request):
     return HttpResponse(request, 'gamer_view/Trending.html')
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'gamer_view/register.html', context = {'user_form' : user_form,
+                                                                    'profile_form' : profile_form,
+                                                                    'registered' : registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return redirect(reverse('gamer_view:Home'))
+            else:
+                return HttpResponse('Your gamer_view account is disabled.')
+        
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        return render(request,'gamer_view/login.html')
