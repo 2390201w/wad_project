6d09ec5c91dfb387e087ab8196b1ce47fadc1c16
Kalok Chan <chankalok@hotmail.co.uk>
2020-04-02 15:38:26 +0100

    added comments to populate_gamer_view.py

--- populate_gamer_view.py
+++ populate_gamer_view.py
@@ -7,6 +7,7 @@ django.setup()
 
 from gamer_view.models import Category ,Page, UserProfile, User ,Reviews
 
+# Cleans the Database
 def clean():
     Category.objects.all().delete()
     Page.objects.all().delete()
@@ -14,24 +15,28 @@ def clean():
     User.objects.all().delete()
     Reviews.objects.all().delete()
 
+# function that adds categories
 def add_cat(name):
     cat=Category.objects.get_or_create(category=name)[0]
     cat.save()
-
+    
+# function that adds games
 def add_page(name, cat,date, desc, image,views):
     page=Page.objects.get_or_create(gamename=name, cat=cat,date_created=date, description=desc, image=image, views=views)[0]
     page.save()
 
-
+# function that adds users
 def add_user(username, pas):
     user=User.objects.get_or_create(username=username,password=pas)[0]
     user.save()
     return user
 
+# function that creates user profiles
 def create_profile(name, image, email):
     user_prof= UserProfile.objects.get_or_create(user=name, picture=image, email=email)[0]
     user_prof.save()
 
+## function that adds reviews
 def add_review(game, review, user, date, rating):
     rev=Reviews.objects.get_or_create(gamename=game, review=review, madeby=user, datecreated=date, rating=rating)[0]
     rev.save()
@@ -40,10 +45,10 @@ def add_review(game, review, user, date, rating):
 
 def populate():
 
-    #list of categories
+    #list of Categories
     category=['FPS', 'MOBA', 'Action', 'MMORPG', 'Strategy', 'Sport', 'Simulator']
 
-    #list of games
+    #list of Games
     games= [{'gamename': 'Call Of Duty', 'category':'FPS','date': '2018-07-10','description': "shoot people",'image': 'game_images/cod.jfif','views':'5'},
             {'gamename': 'League of Legends', 'category':'MOBA','date': '2012-03-20','description': "destroy towers",'image': 'game_images/lol.jpg','views':'50'},
             {'gamename': 'BattleField', 'category':'FPS','date': '2019-09-29','description': "shoot people 2",'image': 'game_images/bat.jpg','views':'10'},
@@ -55,34 +60,36 @@ def populate():
             {'gamename': 'Dota 2', 'category':'MOBA','date': '2015-05-6','description': "destroy towers 2",'image': 'game_images/dota.jpg','views':'35'},
             {'gamename': 'Animal Crossing', 'category':'Simulator','date': '2020-03-10','description': "Making a Village",'image': 'game_images/ac.jpg','views':'25'}]
 
-    #list of users
+    #list of Users
     users= [{'username': 'Sheldon', 'password': 'password1', 'image': 'profile_images/user1.png','email' : "user1@gmail.com"},
             {'username': 'Abigail', 'password': 'password1', 'image': 'profile_images/user2.png','email' : "user2@gmail.com"},
             {'username': 'Ben', 'password': 'password1', 'image': 'profile_images/user3.jfif','email' : "user3@gmail.com"},
             {'username': 'Adam', 'password': 'password1', 'image': 'profile_images/user4.jfif','email' : "user4@gmail.com"},
             {'username': 'Katy', 'password': 'password1', 'image': 'profile_images/user5.png','email' : "user5@gmail.com"},]
 
+    #list of Reviews
     reviews=[{'gamename': 'Bloons Tower Defense', 'review' : 'A good game for passing time and does not require much attention','madeby':'Sheldon', 'date':'2011-01-30', 'rating': '3'},
              {'gamename': 'Animal Crossing', 'review' : 'An excellent game for to escape reality','madeby':'Abigail', 'date':'2020-04-01', 'rating': '4'},
              {'gamename': 'Monster Hunter', 'review' : 'Never gets boring due to the constant new free DLCs','madeby':'Adam', 'date':'2020-02-18', 'rating': '5'},
              {'gamename': 'Dark Souls', 'review' : 'Quiet a alot of exploring and has intresting boss fights','madeby':'Sheldon', 'date':'2011-01-30', 'rating': '4'}]
-             
+
+           
     print("Adding Categories")
     for cat in category:
         add_cat(cat)
-        print("Category:", cat, "added")
+        print("Category:", cat)
 
     print("\nAdding Games")
     for game in games:
         category=Category.objects.get(category=game['category'])
         add_page(game['gamename'], category ,game['date'], game['description'], game['image'], game['views'])
-        print("Game:", game['gamename'], "added")
+        print("Game:", game['gamename'])
 
     print("\nAdding Users")
     for user in users:
         name=add_user(user['username'], user['password'])
         create_profile(name, user['image'], user['email'])
-        print("User:", user['username'], "added")
+        print("User:", user['username'])
 
     print("\nAdding Reviews")
     for review in reviews:
@@ -93,8 +100,8 @@ def populate():
         
             
 if __name__=='__main__':
+    print('\nPopulating gamer_view database...\n')
     clean()
-    print('populating...\n')
     populate()
     print('\n---Finished---')
                   
