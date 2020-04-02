772af802f460d35c917d2c20fa089cbd21e51e6f
Leeon Lau <2392383L@student.gla.ac.uk>
2020-04-02 16:30:24 +0100

    Updated formatting of templates

--- gamer_view/forms.py
+++ gamer_view/forms.py
@@ -3,13 +3,45 @@ from gamer_view.models import Page, Category, UserProfile
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'style': 'height:22.5px; position:relative; top:-6px; font-size:16px; width:205px'
+            }
+        )                       
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'style': 'height:22.5px; position:relative; top:-6px; font-size:16px; width:211px'
+            }
+        )                       
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('username', 'password',)
+        help_texts = {
+            'username': None,
+        }
 
 class UserProfileForm(forms.ModelForm):
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                'style': 'height:22.5px; position:relative; top:-6px; font-size:16px; width:287px'
+            }
+        )
+    )
+
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'style': 'height:22.5px; position:relative; top:-6px; font-size:16px;'
+            }
+        )
+    )
+    
     class Meta:
         model = UserProfile
-        fields = ('picture',)
+        fields = ('email', 'picture')
