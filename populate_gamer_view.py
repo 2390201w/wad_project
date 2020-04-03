139f145b9aa865863ae7f64cb738d316cddf6313
Kalok Chan <chankalok@hotmail.co.uk>
2020-04-03 15:55:39 +0100

    added more games and removed spaces in names for populate_gamer_view.py

--- populate_gamer_view.py
+++ populate_gamer_view.py
@@ -46,19 +46,24 @@ def add_review(game, review, user, date, rating):
 def populate():
 
     #list of Categories
-    category=['FPS', 'MOBA', 'Action', 'MMORPG', 'Strategy', 'Sport', 'Simulator']
+    category=['FPS', 'MOBA', 'Action', 'MMORPG', 'Strategy', 'Sport', 'Simulator', 'Racing']
 
     #list of Games
-    games= [{'gamename': 'Call Of Duty', 'category':'FPS','date': '2018-07-10','description': "shoot people",'image': 'game_images/cod.jfif','views':'5'},
-            {'gamename': 'League of Legends', 'category':'MOBA','date': '2012-03-20','description': "destroy towers",'image': 'game_images/lol.jpg','views':'50'},
+    games= [{'gamename': 'CallOfDuty', 'category':'FPS','date': '2018-07-10','description': "shoot people",'image': 'game_images/cod.jfif','views':'5'},
             {'gamename': 'BattleField', 'category':'FPS','date': '2019-09-29','description': "shoot people 2",'image': 'game_images/bat.jpg','views':'10'},
-            {'gamename': 'Monster Hunter', 'category':'Action','date': '2020-01-12','description': "Hunt monsters",'image': 'game_images/MH.jfif','views':'15'},
-            {'gamename': 'Dark Souls', 'category':'Action','date': '2011-12-1','description': "Gather souls",'image': 'game_images/ds.jpg','views':'30'},
-            {'gamename': 'Fifa 18', 'category':'Sport','date': '2018-11-29','description': "Score goals",'image': 'game_images/fifa.jpg','views':'8'},
-            {'gamename': 'Bloons Tower Defense', 'category':'Strategy','date': '2010-02-26','description': "Pop balloons and defend base",'image': 'game_images/btd.jpg','views':'20'},
-            {'gamename': 'World of Warcraft', 'category':'MMORPG','date': '2005-04-10','description': "Go on a legendary adventure",'image': 'game_images/wow.jpg','views':'40'},
-            {'gamename': 'Dota 2', 'category':'MOBA','date': '2015-05-6','description': "destroy towers 2",'image': 'game_images/dota.jpg','views':'35'},
-            {'gamename': 'Animal Crossing', 'category':'Simulator','date': '2020-03-10','description': "Making a Village",'image': 'game_images/ac.jpg','views':'25'}]
+            {'gamename': 'CSGO', 'category':'FPS','date': '2014-02-03','description': "shoot people 3",'image': 'game_images/csgo.jfif','views':'18'},
+            {'gamename': 'LeagueOfLegends', 'category':'MOBA','date': '2012-03-20','description': "destroy towers",'image': 'game_images/lol.jpg','views':'50'},
+            {'gamename': 'Dota2', 'category':'MOBA','date': '2015-05-06','description': "destroy towers 2",'image': 'game_images/dota.jpg','views':'35'},
+            {'gamename': 'Smite', 'category':'MOBA','date': '2016-08-15','description': "destroy towers 3",'image': 'game_images/smite.jpg','views':'30'},
+            {'gamename': 'MonsterHunter', 'category':'Action','date': '2020-01-12','description': "Hunt monsters",'image': 'game_images/MH.jfif','views':'45'},
+            {'gamename': 'DarkSouls', 'category':'Action','date': '2011-12-01','description': "Gather souls",'image': 'game_images/ds.jpg','views':'30'},
+            {'gamename': 'TheWitcher3', 'category':'Action','date': '2018-04-23','description': "Story driven",'image': 'game_images/witcher.jfif','views':'40'},
+            {'gamename': 'Fifa18', 'category':'Sport','date': '2018-11-29','description': "Play Football",'image': 'game_images/fifa.jpg','views':'8'},
+            {'gamename': 'NBA2K20', 'category':'Sport','date': '2019-12-23','description': "Play Basketball",'image': 'game_images/nba.jpg','views':'24'},
+            {'gamename': 'BloonsTowerDefense', 'category':'Strategy','date': '2010-02-26','description': "Pop balloons and defend base",'image': 'game_images/btd.jpg','views':'20'},
+            {'gamename': 'Hearthstone', 'category':'Strategy','date': '2016-05-03','description': "Turn based card game",'image': 'game_images/hearthstone.jpg','views':'28'},
+            {'gamename': 'WorldOfWarcraft', 'category':'MMORPG','date': '2005-04-10','description': "Go on a legendary adventure",'image': 'game_images/wow.jpg','views':'40'},
+            {'gamename': 'AnimalCrossing', 'category':'Simulator','date': '2020-03-10','description': "Making a Village",'image': 'game_images/ac.jpg','views':'25'}]
 
     #list of Users
     users= [{'username': 'Sheldon', 'password': 'password1', 'image': 'profile_images/user1.png','email' : "user1@gmail.com"},
@@ -68,10 +73,10 @@ def populate():
             {'username': 'Katy', 'password': 'password1', 'image': 'profile_images/user5.png','email' : "user5@gmail.com"},]
 
     #list of Reviews
-    reviews=[{'gamename': 'Bloons Tower Defense', 'review' : 'A good game for passing time and does not require much attention','madeby':'Sheldon', 'date':'2011-01-30', 'rating': '3'},
-             {'gamename': 'Animal Crossing', 'review' : 'An excellent game to escape reality','madeby':'Abigail', 'date':'2020-04-01', 'rating': '4'},
-             {'gamename': 'Monster Hunter', 'review' : 'Never gets boring due to the constant new free DLCs','madeby':'Adam', 'date':'2020-02-18', 'rating': '5'},
-             {'gamename': 'Dark Souls', 'review' : 'Quiet a lot of exploring and has intresting boss fights','madeby':'Sheldon', 'date':'2011-01-30', 'rating': '4'}]
+    reviews=[{'gamename': 'BloonsTowerDefense', 'review' : 'A good game for passing time and does not require much attention','madeby':'Sheldon', 'date':'2011-01-30', 'rating': '3'},
+             {'gamename': 'AnimalCrossing', 'review' : 'An excellent game to escape reality','madeby':'Abigail', 'date':'2020-04-01', 'rating': '4'},
+             {'gamename': 'MonsterHunter', 'review' : 'Never gets boring due to the constant new free DLCs','madeby':'Adam', 'date':'2020-02-18', 'rating': '5'},
+             {'gamename': 'DarkSouls', 'review' : 'Quiet a lot of exploring and has intresting boss fights','madeby':'Sheldon', 'date':'2011-01-30', 'rating': '4'}]
 
            
     print("Adding Categories")
