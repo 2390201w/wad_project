f8484a2b8f8b3eddccb45264d0b8ffacee083815
Leeon Lau <2392383L@student.gla.ac.uk>
2020-04-08 14:28:09 +0100

    reformatted username error message on the register page

--- gamer_view/forms.py
+++ gamer_view/forms.py
@@ -40,8 +40,7 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
-
+        fields = ('username', 'email', 'password')
 
 class UserProfileForm(forms.ModelForm):
     
